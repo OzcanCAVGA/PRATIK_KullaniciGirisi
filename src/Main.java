33906174815eb2902355b7bc5cf25e1534a75be3
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2022-09-29 20:11:44 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
+/*
+Eğer şifre yanlış ise kullanıcıya şifresini sıfırlayıp sıfırlamayacağını sorun,
+eğer kullanıcı sıfırlamak isterse yeni girdiği şifrenin hatalı girdiği ve unuttuğu şifre
+ile aynı olmaması gerektiğini kontrol edip , şifreler aynı ise ekrana "Şifre oluşturulamadı,
+lütfen başka şifre giriniz." sorun yoksa "Şifre oluşturuldu" yazan programı yazınız.
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner yazici = new Scanner(System.in);
+
+        String userName, password, newPassword, answer;
+
+        System.out.println("Lutfen kullanici adinizi girin: ");
+        userName = yazici.nextLine();
+
+        System.out.println("Lutfen sifrenizi girin: ");
+        password = yazici.nextLine();
+
+        if (userName.equals("patika") && password.equals("java")) {
+            System.out.println("Tebrikler, basariyla giris yaptiniz!");
+        } else {
+            System.out.println("Sifreniz yanlis, tekrar olusturmak ister misiniz?(e/h) ");
+            answer = yazici.nextLine();
+            if (answer.equals("e")) {
+                System.out.println("Lutfen yeni sifrenizi giriniz: ");
+                newPassword = yazici.nextLine();
+                if (!(newPassword.equals("java") && newPassword.equals(password)))
+                    System.out.println("Sifre olusturulamadi, lutfen baska sifre giriniz.");
+                else
+                    System.out.println("Sifre basariyla olusturuldu.");
+
+            } else
+                System.out.println("Sistemden basariyla cikildi.");
+        }
+
+
     }
 }
